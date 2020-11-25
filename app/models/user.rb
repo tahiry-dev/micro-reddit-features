8fce99074b007ddd7556788f03eea93e4c3e812a
tahiry-dev <tahirycontact@gmail.com>
2020-11-25 09:20:04 +0300

    add validation to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :login, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, uniqueness: true
 end
