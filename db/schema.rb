637bc8061170bf5bfaff18fac79cf678969eeec7
tahiry-dev <tahirycontact@gmail.com>
2020-11-25 09:23:51 +0300

    create a model for Post and migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_052137) do
+ActiveRecord::Schema.define(version: 2020_11_25_062153) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login"
